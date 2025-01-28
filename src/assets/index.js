4b9fb22a919f4c31a51bedc1ad6db7d626f4ad2a
RinkalYadav <rinkalbyadav@gmail.com>
2025-01-28 16:27:45 +0530

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,89 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+// our image
+import dklogo from "./dklogo.jpeg";
+import dklogo2 from "./rinkal.jpeg";
+import front from "./front.png";
+// our technology image
+import clang from "./tech/clang.jpg";
+import clang2 from "./tech/clang2.jpg";
+import dsa from "./tech/dsa.png";
+import java from "./tech/java.jpg";
+import python from "./tech/python.jpg";
+import sql from "./tech/sql.jpg";
+import spring from "./tech/spring.png";
+import bootstrap from "./tech/bootstrap.jpg";
+import react from "./tech/reactjsframe.png";
+import css from "./tech/css.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+
+// our company image
+import destination from "./company/download.png";
+import hackerrank from "./hackerrank.png";
+
+//our projects image
+import chatgpt from "./chatgpt.png";
+import currency from "./currency.png";
+import library from "./library.jpg";
+import alexa from "./alexa.png";
+import spotify from "./spotify.jpg"
+import amazon from "./amazon.png"
+// our certification image
+import java_basic from "./java_basic.png";
+import sql_basic from "./SQL_basic.png";
+import sql_intermediate from "./SQL_Intermidiate.png";
+import javascript_basic from "./javascript_basic.png";
+import sql_advance from "./sql_advance.png";
+import css_basic from "./css_basic.png";
+
+
+export {
+  logo,
+  backend,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  html,
+  javascript,
+  reactjs,
+  tailwind,
+
+  dklogo,
+  dklogo2,
+  front,
+  destination,
+  clang,
+  clang2,
+  java,
+  python,
+  sql,
+  spring,
+  bootstrap,
+  dsa,
+  react,
+
+  chatgpt,
+  currency,
+  library,
+  java_basic,
+  sql_basic,
+  sql_intermediate,
+  hackerrank,
+  javascript_basic,
+  css_basic,
+  sql_advance,
+  alexa,
+  spotify,
+  amazon,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,356 @@
+
+import {
+  mobile,
+  backend,
+
+  web,
+  javascript,
+  html,
+  css,
+  tailwind,
+
+
+  front,
+  destination,
+  clang,
+  clang2,
+  java,
+  python,
+  sql,
+  spring,
+  bootstrap,
+  dsa,
+  react,
+  chatgpt,
+  currency,
+  library,
+  java_basic,
+  sql_basic,
+  sql_intermediate,
+  javascript_basic,
+  sql_advance,
+  css_basic,
+  alexa,
+  spotify,
+  amazon,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+ 
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React  Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Frontend developer",
+    icon: front,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML",
+    icon: html,
+  },
+  {
+    name: "CSS",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "c language",
+    icon: clang,
+  },
+  {
+    name: "React JS",
+    icon: react,
+  },
+  {
+    name: "c++ language",
+    icon: clang2,
+  },
+  {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "sql",
+    icon: tailwind,
+  },
+  {
+    name: "bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "java",
+    icon: java,
+  },
+  {
+    name: "data structure",
+    icon: dsa,
+  },
+  {
+    name: "spring",
+    icon: spring,
+  },
+  {
+    name: "bootstrap",
+    icon: sql,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full stack Developer",
+    company_name: "Destination Technology",
+    icon: destination,
+    iconBg: "#E6DEDD",
+    date: "Sep 2024 - March 2025",
+    points: [
+      "As a full-stack developer, I enjoy building scalable and efficient web applications from front to back. With expertise in Java, Spring Boot, MySQL, and modern front-end technologies like React and Angular, I focus on creating seamless user experiences and robust backend systems. I thrive in collaborative environments, writing clean, maintainable code, and staying updated with the latest tech trends to deliver high-quality solutions.",
+    ],
+  },
+  {
+    title: "Java Developer",
+    company_name: "Destination Technology",
+    icon: destination,
+    iconBg: "#E6DEDD",
+    date: "Sep 2024 - March 2025",
+    points: [
+      "I’m a passionate Java developer with a strong foundation in building scalable and efficient web applications. I specialize in Java, Spring Boot, Hibernate, and MySQL, focusing on writing clean, maintainable code and optimizing application performance. I enjoy collaborating with cross-functional teams, integrating RESTful APIs, and ensuring seamless backend functionality. With a problem-solving mindset, I strive to develop robust, secure, and high-quality applications while staying updated with the latest advancements in Java and backend technologies.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Destination Technology",
+    icon: destination,
+    iconBg: "#383E56",
+    date: "Sep 2024 - March 2025",
+    points: [
+      "I’m a dedicated web developer with a passion for creating responsive, fast, and visually appealing websites. Skilled in HTML, CSS, JavaScript, and modern frameworks, I focus on building seamless user experiences and maintaining clean, efficient code. I enjoy collaborating with teams to develop scalable web solutions while staying updated with the latest industry trends.",
+    ],
+  },
+  {
+    title: "React.js Developer",
+    company_name: "Destination Technology",
+    icon: destination,
+    iconBg: "#383E56",
+    date: "Sep 2024 - March 2025",
+    points: [
+      "I’m a passionate React.js developer with a keen eye for building dynamic and user-friendly web applications. I enjoy creating reusable components, optimizing performance, and ensuring seamless user experiences across different devices and browsers. With a strong foundation in JavaScript, HTML, and CSS, I love collaborating with designers and backend developers to bring ideas to life. Writing clean, maintainable code and staying up-to-date with the latest trends in the React ecosystem is something I prioritize to build high-quality, scalable applications.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    hackerranklink:
+      "https://www.hackerrank.com/certificates/ff75e0e45aa9",
+    name: "Java basic",
+    designation: "From",
+    company: "Hacker rank",
+    image: java_basic,
+  },
+  {
+    hackerranklink:
+      "https://www.hackerrank.com/certificates/4b79ace354a7",
+    name: "SQL basic",
+    designation: "From",
+    company: "Hacker rank",
+    image: sql_basic,
+  },
+  {
+    hackerranklink:
+      "https://www.hackerrank.com/certificates/3aeb1770c03c",
+    name: "SQL Intermediate",
+    designation: "From",
+    company: "Hacker rank",
+    image: sql_intermediate,
+  },
+  {
+    hackerranklink:
+      "https://www.hackerrank.com/certificates/0371a0879388",
+    name: "CSS Basic",
+    designation: "From",
+    company: "Hacker rank",
+    image: css_basic,
+  },
+  {
+    hackerranklink:
+      "https://www.hackerrank.com/certificates/b893f911604f",
+    name: "SQL Advanced",
+    designation: "From",
+    company: "Hacker rank",
+    image: sql_advance,
+  },
+  {
+    hackerranklink:
+      "https://www.hackerrank.com/certificates/8fdd217029aa",
+    name: "JavaScript Basic",
+    designation: "From",
+    company: "Hacker rank",
+    image: javascript_basic,
+  },
+];
+
+const projects = [
+  {
+    name: "Virtual Assistant",
+    description:
+      "The Virtual Assistance project is a web-based interactive platform, built using HTML, CSS, and JavaScript, designed to provide users with quick responses and assistance for various tasks. The project emulates the functionality of a personal assistant and was developed to enhance user experience through an intuitive interface and seamless interaction.",
+    tags: [
+      {
+        name: "Html&CSS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "API",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: alexa,
+    source_code_link: "https://github.com/RinkalYadav/VitrualAssistance",
+  },
+  {
+    name: "Music Player",
+    description:
+      "The Spotify Music Player Clone is a web-based application, built using HTML, CSS, and JavaScript, designed to replicate the core functionality and interface of a music player. The project provides users with an interactive platform to play, pause, and navigate through tracks, offering a seamless and engaging digital music experience for all types of users.",
+    tags: [
+      {
+        name: "Html&CSS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: spotify,
+    source_code_link: "https://github.com/RinkalYadav/Spotify",
+  },
+  {
+    name: "Amazon Clone",
+    description:
+      "The Amazon Clone is a web-based application, developed using HTML, CSS, and JavaScript, designed to replicate the core features and interface of the Amazon shopping platform. The project offers users an intuitive online shopping experience, allowing them to browse products, view details, while providing a seamless and engaging e-commerce interface.",
+    tags: [
+      {
+        name: "Html&CSS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "API",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: amazon,
+    source_code_link: "https://github.com/RinkalYadav/Amazon_clone",
+  },
+  {
+    name: "Currency Converter",
+    description:
+      "I developed a currency converter web app that provides real-time currency conversion using live exchange rates. Built with JavaScript, React.js, and API integration, it ensures accuracy and a smooth user experience. This project highlights my skills in front-end development and API handling.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "API",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: currency,
+    source_code_link: "https://github.com/RinkalYadav",
+  },
+  {
+    name: "Mini Chatgpt",
+    description:
+      "I developed a mini ChatGPT application using React, Tailwind CSS, and API integration to provide AI-powered responses. The app features a sleek UI and ensures a smooth user experience with real-time interactions. This project showcases my skills in front-end development, API handling, and UI design.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Html&CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "API",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: chatgpt,
+    source_code_link: "https://github.com/RinkalYadav",
+  },
+  {
+    name: "Library Management",
+    description:
+      "Developed a LMS using HTML, CSS, JavaScript, and PHP for efficient book and user management. It features a responsive design and smooth functionality for lending operations. This project highlights my skills in web development, database integration, and UI design.",
+    tags: [
+      {
+        name: "Html&CSS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Javascript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "SQL&PHP",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: library,
+    source_code_link: "https://github.com/RinkalYadav",
+  },
+  
+  
+];
+
+export { services, technologies, experiences, testimonials, projects };
+
+
+
